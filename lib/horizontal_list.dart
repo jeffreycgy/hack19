1926f028aeb9e5c03574613018980d3d879f93e3
Yong Ching <reachyongching@gmail.com>
2019-06-01 15:29:42 +0800

    With ugly tags ui

--- lib/horizontal_list.dart
+++ lib/horizontal_list.dart
@@ -11,38 +11,18 @@ class HorizontalTags extends StatefulWidget {
 }
 
 class _HorizontalList extends State<HorizontalTags> {
-  // @override
-  // Widget build(BuildContext context) {
-  //   return Container(
-  //     margin: EdgeInsets.symmetric(vertical: 20.0),
-  //     height: 200.0,
-  //     child: ListView(
-  //       scrollDirection: Axis.horizontal,
-  //       children: <Widget>[
-  //         Container(
-  //           width: 160.0,
-  //           color: Colors.red,
-  //         ),
-  //         Container(
-  //           width: 160.0,
-  //           color: Colors.blue,
-  //         ),
-  //         Container(
-  //           width: 160.0,
-  //           color: Colors.green,
-  //         ),
-  //         Container(
-  //           width: 160.0,
-  //           color: Colors.yellow,
-  //         ),
-  //         Container(
-  //           width: 160.0,
-  //           color: Colors.orange,
-  //         ),
-  //       ],
-  //     ),
-  //   );
-  // }
+  Container getTagWidgets(String name) {
+    return new Container(
+        height: 30.0,
+        width: 200,
+        margin: const EdgeInsets.only(left: 5.0, right: 5.0),
+        decoration: new BoxDecoration(
+            color: Colors.deepPurple[200],
+            borderRadius: new BorderRadius.only(
+                topLeft: const Radius.circular(3.0),
+                topRight: const Radius.circular(3.0))),
+        child: Column(children: <Widget>[Text(name)]));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -56,16 +36,12 @@ class _HorizontalList extends State<HorizontalTags> {
             print(json.encode(snapshot.data));
             return Container(
               margin: EdgeInsets.symmetric(vertical: 20.0),
-              height: 10.0,
+              height: 30.0,
+              width: 250,
               child: ListView(
                 scrollDirection: Axis.horizontal,
                 children: <Widget>[
-                  Container(
-                    height: 10.0,
-                    width: 45.0,
-                    margin: const EdgeInsets.only(left: 5.0, right: 5.0),
-                    color: Colors.deepPurple[200],
-                  ),
+                  for (var item in snapshot.data.items) getTagWidgets(item.name)
                 ],
               ),
             );
